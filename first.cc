4a9ea85fd2fa4cf5016f38345b56a0404de2963d
Gavi Narra <gavi.narra@iprocedures.com>
2016-09-08 11:01:18 -0400

    Initial commit

---
+++ first.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x,y;
+	cout <<"Enter two numbers:";
+	cin >> x >> y;
+	int sum = x + y;
+	cout <<"Their sum is "<< sum <<endl;
+	return EXIT_SUCCESS;
+}
