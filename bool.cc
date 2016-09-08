08fb92aaa8b6d84500a1a1cf622c90578f2fe9d2
Gavi Narra <gavi.narra@iprocedures.com>
2016-09-08 11:30:23 -0400

    basic boolean

---
+++ bool.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	bool a;
+	a=true;
+	if(a){
+		cout <<"variable a is true" <<endl;	
+	}
+	else{
+		cout <<"variable b is false" <<endl;
+	}
+	return EXIT_SUCCESS;
+}
