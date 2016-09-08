e0101b6f9fcc49e5ede218358e527cdca1474a9a
Gavi Narra <gavi.narra@iprocedures.com>
2016-09-08 11:26:08 -0400

    basic floar

---
+++ float.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float a=123;
+	double b=123.344;
+	cout << a << '\t' << b <<endl;
+	cout <<"Size of float: "<<sizeof(a) <<endl;
+	cout <<"Size of double: " <<sizeof(b) <<endl;
+	return EXIT_SUCCESS;
+}
