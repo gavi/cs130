734c8df0bc97379552a228fbb577e9d0b87f29cf
Gavi Narra <gavi.narra@iprocedures.com>
2016-09-08 12:06:31 -0400

    pointer arithmatic different types

--- arrayptr.cc
+++ arrayptr.cc
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-	int arr[3];
-	arr[0] = 10;
-	arr[1] = 20;
-	arr[2] = 30;
+	int arr[2]={10,20};
 	int *x = arr;
 	cout << *(x+1) <<endl;
+	char car[2]={'a','b'};
+	char *p=car;
+	cout << *(p+1) <<endl;
 	return EXIT_SUCCESS;
 }
