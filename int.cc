68202a333b73c4914ae84f7ae3a8b8eefbe2026f
Gavi Narra <gavi.narra@iprocedures.com>
2016-09-08 11:20:12 -0400

    int short long

---
+++ int.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a=10;
+	short b=200;
+	long c=500; 
+	cout << "Int size:" << sizeof(a) << endl;
+	cout << "Short size:" << sizeof(b) << endl;
+	cout << "Long size:" << sizeof(c) << endl;
+	return EXIT_SUCCESS;
+}
