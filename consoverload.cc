c704f81a2688df6c0d9910afab30e7043e5ca1ba
Gavi Narra <gavi.narra@iprocedures.com>
2016-09-13 12:24:12 -0400

    constructor overload

---
+++ consoverload.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Point{
+	int x,y;
+	public:
+	Point();
+	Point(int,int);
+	void print();
+};
+Point::Point(){
+	x=10;
+	y=10;
+}
+
+Point::Point(int a, int b){
+	x=a;
+	y=b;
+}
+
+void Point::print(){
+	cout<<"("<<x<<","<<y<<")"<<endl;
+}
+
+int main(){
+	Point *a=new Point();
+	a->print();
+	delete a;
+}
